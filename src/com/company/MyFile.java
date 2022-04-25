ad4c041c014cf5aec43b90a76061b16143ef79de
Nursulu Ospan <nursulu.ospan@jits-3-20.jits.ipa.dc>
2022-04-25 13:22:55 +0600

    second commit

--- src/com/company/MyFile.java
+++ src/com/company/MyFile.java
@@ -1,27 +1,23 @@
 package com.company;
 import java.io.*;
+import java.nio.file.Files;
 import java.util.Scanner;
 
 public class MyFile {
-    //String[] myFiles;
     // выводит список всех файлов и директорий для `path` - ls
     // stdin: ls ../
     // stdout: dir1 dir2 hello_world.py .secret_file
     public static void listDirectory(String path) {
         try {
-            if (path.contains("ls")) {
-                String[] subPath;
-                subPath = path.split(" ");
-                File folder = new File(subPath[1]);
-
+            File folder = new File(path);
+            if(folder.exists()) {
                 for (File file : folder.listFiles()) {
                     System.out.print(file.getName() + " ");
                 }
                 System.out.println();
-            } if(!path.contains("help") & !path.contains("exit") & !path.contains("") ) {
-                System.out.println("Incorrect command");
             }
-
+            else { System.out.println("File not exist");
+            }
         } catch (Exception e) {
             System.out.printf("%s", e);
             return;
@@ -30,30 +26,137 @@ public class MyFile {
 
     // выводит список файлов с расширением `.py` в `path` - ls_py
     public static void listPythonFiles(String path) {
+        try {
+            String[] myFiles = new String[]{};
+            File folder = new File(path);
+            if(folder.exists()) {
+                myFiles = folder.list();
+                for (String file : myFiles) {
+                    if (file.contains(".py")) {
+                        System.out.print(file + ' ');
+                    }
+                }
+                System.out.println();
+            }
+            else { System.out.println("File not exist");
+            }
+        } catch (Exception e) {
+            System.out.printf("%s", e);
+            return;
+        }
     }
     // выводит `true`, если `path` это директория, в других случаях `false` - id_dir
     public static void isDirectory(String path) {
+        try {
+            File folder = new File(path);
+            if(folder.exists()) {
+                    /*
+                    for (File file : folder.listFiles()) {
+                        System.out.print(file.getName() + " -> " + file.isDirectory() + " ");
+                    }
+                    */
+                System.out.println(folder.isDirectory());
+            }
+            else { System.out.println("File not exist");
+            }
 
+        } catch (Exception e) {
+            System.out.printf("%s", e);
+            return;
+        }
     }
     // выводит `директория` или `файл` в зависимости от типа `path` - define
     public static void define(String path) {
-
+        try {
+            File folder = new File(path);
+            if(folder.exists()) {
+                    /*
+                    for (File file : folder.listFiles()) {
+                        System.out.print(file.getName() + " -> " + file.isDirectory() + " ");
+                    }
+                    */
+                boolean isDir = folder.isDirectory();
+                boolean isFile = folder.isFile();
+                if (isDir == true) { System.out.println(folder.getName() + " is Directory"); }
+                if (isFile == true) { System.out.println(folder.getName() + " is File"); }
+            }
+            else { System.out.println("File not exist");
+            }
+        } catch (Exception e) {
+            System.out.printf("%s", e);
+            return;
+        }
     }
     // выводит права для файла в формате `rwx` для текущего пользователя - readmod
     public static void printPermissions(String path) {
-
+        try {
+            String filePermissions = "";
+            File folder = new File(path);
+            if(folder.exists()) {
+                if (folder.canRead()) filePermissions += "r";
+                else filePermissions += "-";
+                if (folder.canWrite()) filePermissions += "w";
+                else filePermissions += "-";
+                if (folder.canExecute()) filePermissions += "x";
+                else filePermissions += "-";
+                System.out.println(filePermissions);
+            } else { System.out.println("File not exist");
+            }
+        } catch (Exception e) {
+            System.out.printf("%s", e);
+            return;
+        }
     }
     // устанавливает права для файла `path` - setmod
     public static void setPermissions(String path, String permissions) {
-
+        try {
+            String[] filePermissions = permissions.split("");
+            File folder = new File(path);
+            if(folder.exists()) {
+                if (filePermissions[0].contains("r")) folder.setReadable(true);
+                else folder.setReadable(false);
+                if (filePermissions[1].contains("w")) folder.setWritable(true);
+                else folder.setWritable(false);
+                if (filePermissions[2].contains("x")) folder.setExecutable(true);
+                else folder.setExecutable(false);
+            } else { System.out.println("File not exist");
+            }
+        } catch (Exception e) {
+            System.out.printf("%s", e);
+            return;
+        }
     }
+
     // выводит контент файла - cat
     public static void printContent(String path) {
+        try { // 1
+            File folder = new File(path);
+            if(folder.exists()) {
+                Scanner obj = new Scanner(folder);
+                while (obj.hasNextLine()) {
+                    System.out.println(obj.nextLine());
+                }
+            } else { System.out.println("File not exist");
+            }
 
+        } catch (Exception e) {
+            System.out.printf("%s", e);
+            return;
+        }
     }
     // добавляет строке `# Autogenerated line` в конец `path` - append
     public static void appendFooter(String path) {
-
+        try {
+            FileWriter file = new FileWriter(path);
+            //if(file.exists()) {
+                String line = "# Autogenerated line";
+                file.write(line + "\n");
+            //} else { System.out.println("File not exist");
+           // }
+        } catch (Exception e) { // 8
+            System.out.printf("%s", e);
+            return;
+        }
     }
     // создает копию `path` в директорию `/tmp/${date}.backup` где, date - это дата в формате `dd-mm-yyyy`. `path` может быть директорией или файлом. При директории, копируется весь контент. - bc
     public static void createBackup(String path) {
@@ -77,7 +180,7 @@ public class MyFile {
                 "bc <path>               создает копию `path` в директорию `/tmp/${date}.backup` где, date - это дата в формате `dd-mm-yyyy`\n" +
                 "greplong <path>         выводит самое длинное слово в файле\n" +
                 "help                    выводит список команд и их описание\n" +
-                "exit                    завершает работу программы\n");
+                "exit                    завершает работу программы");
     }
     // завершает работу программы - exit
     public static void exit() {
@@ -98,17 +201,26 @@ public class MyFile {
                 "bc <path>               создает копию `path` в директорию `/tmp/${date}.backup` где, date - это дата в формате `dd-mm-yyyy`\n" +
                 "greplong <path>         выводит самое длинное слово в файле\n" +
                 "help                    выводит список команд и их описание\n" +
-                "exit                    завершает работу программы\n");
+                "exit                    завершает работу программы");
         while(true){
-            Scanner scanner = new Scanner(System.in); // 2
+            Scanner scanner = new Scanner(System.in);
             System.out.printf("> ");
-            String line = scanner.nextLine(); // 4
-            //if (line!="") {
-                listDirectory(line);
-                if (line.contains("exit")) { exit(); }
-                if (line.contains("help")) { help(); }
-           // }
-
+            String line = scanner.nextLine();
+            String[] subPath;
+            subPath = line.split(" ");
+            if(subPath.length > 1) {
+                if (line.contains("ls") && !line.contains("ls_py")) listDirectory(subPath[1]);
+                if (line.contains("ls_py")) listPythonFiles(subPath[1]);
+                if (line.contains("is_DIR")) isDirectory(subPath[1]);
+                if (line.contains("define")) define(subPath[1]);
+                if (line.contains("readmod")) printPermissions(subPath[1]);
+                if (line.contains("setmod")) setPermissions(subPath[1], subPath[2]);
+                if (line.contains("cat")) printContent(subPath[1]);
+                if (line.contains("append")) appendFooter(subPath[1]);
+                if (line.contains("help")) help();
+                if (line.contains("exit")) exit();
+            }
+            else {System.out.println("Path not exist");}
         }
     }
 
